59f093752bda85b406e0aa074c7de8b68da8b706
Pamela Santos <pamesantoss@hotmail.com>
2024-09-03 20:25:07 +0000

    feat: creating the issue card component

---
+++ src/components/CardIssue/index.tsx
@@ -0,0 +1,19 @@
+export function CardIssue() {
+  return (
+    <div className="bg-base-post rounded-lg p-8 max-h-64">
+      <div className="flex items-baseline">
+        <h2 className="font-nunito text-base-title text-xl font-semibold flex-1 pr-6">
+          JavaScript data types and data structures
+        </h2>
+        <span className="font-nunito text-base-span text-sm">Há 1 dia</span>
+      </div>
+      <p className="font-nunito text-base-text mt-4 text-ellipsis overflow-hidden max-h-[100px]">
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in JavaScript and what properties
+        they have. These can be used to build other data structures. Wherever
+        possible, comparisons with other languages are drawn.
+      </p>
+    </div>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,19 @@
 import { CardIntro } from "../../components/CardIntro"
+import { CardIssue } from "../../components/CardIssue"
 
 export function Home() {
-  return <CardIntro />
+  return (
+    <>
+      <CardIntro />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
+        <CardIssue />
+        <CardIssue />
+        <CardIssue />
+        <CardIssue />
+        <CardIssue />
+        <CardIssue />
+      </div>
+    </>
+  )
 }
