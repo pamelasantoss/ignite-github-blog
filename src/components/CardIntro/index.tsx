3b67ce6fa6f696ab674277a33e67a69d34ecabc9
Pamela Santos <pamesantoss@hotmail.com>
2024-09-11 21:11:24 +0000

    feat: starting fetch user data from github api

--- src/components/CardIntro/index.tsx
+++ src/components/CardIntro/index.tsx
@@ -4,61 +4,87 @@ import {
   Building,
   Users
 } from "@phosphor-icons/react"
+import { githubAPI } from "../../lib/axios"
+import { useEffect, useState } from "react"
+
+interface UserInfo {
+  avatar_url: string
+  name: string
+  bio: string
+  login: string
+  company: string | null
+  followers: number
+}
 
 export function CardIntro() {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null)
+
+  const fetchUserInfo = async () => {
+    const user = import.meta.env.VITE_GITHUB_USER
+    const response = await githubAPI.get(`/users/${user}`)
+    setUserInfo(response.data)
+  }
+
+  const goToGithubPage = () => {
+    console.log("Go to: ", `https://github.com/${userInfo?.login}`)
+  }
+
+  useEffect(() => {
+    fetchUserInfo()
+  }, [])
+
+  const showUserImage = userInfo?.avatar_url
+    ? userInfo?.avatar_url
+    : "https://placehold.co/148x148"
+
   return (
     <div className="bg-base-profile rounded-xl drop-shadow-md -mt-20 flex flex-col md:flex-row justify-between items-start md:items-center gap-6 p-6 relative">
-      <button className="absolute top-6 right-6 font-nunito text-primary-blue text-xs font-semibold uppercase flex items-center gap-1 pb-1 border-b border-transparent hover:border-primary-blue">
+      <button
+        onClick={goToGithubPage}
+        className="absolute top-6 right-6 font-nunito text-primary-blue text-xs font-semibold uppercase flex items-center gap-1 pb-1 border-b border-transparent hover:border-primary-blue"
+      >
         Github
         <ArrowSquareUpRight size={20} />
       </button>
 
       <div className="min-w-36">
-        <img src="https://placehold.co/148x148" alt="" className="rounded-lg" />
+        <img
+          src={showUserImage}
+          alt=""
+          className="rounded-lg"
+          width={148}
+          height={148}
+        />
       </div>
 
-      <div>
+      <div className="flex-1">
         <h1 className="font-nunito text-base-title text-2xl font-semibold">
-          Cameron Williamson
+          {userInfo?.name}
         </h1>
-        <p className="font-nunito text-base-text my-2">
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        {userInfo?.bio && (
+          <p className="font-nunito text-base-text my-2">{userInfo?.bio}</p>
+        )}
         <ul className="flex flex-col md:flex-row gap-1 md:gap-7 pt-3">
           <li>
             <a
-              href="#"
+              href={`https://github.com/${userInfo?.login}`}
               target="_blank"
-              title=""
+              title={userInfo?.login}
               className="font-nunito text-base-subtitle flex items-center gap-2 hover:underline"
             >
               <GithubLogo size={18} weight="fill" className="text-base-label" />
-              cameronwll
+              {userInfo?.login}
             </a>
           </li>
-          <li>
-            <a
-              href="#"
-              target="_blank"
-              title=""
-              className="font-nunito text-base-subtitle flex items-center gap-2 hover:underline"
-            >
+          {userInfo?.company && (
+            <li className="font-nunito text-base-subtitle flex items-center gap-2">
               <Building size={18} weight="fill" className="text-base-label" />
-              Rocketseat
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              target="_blank"
-              title=""
-              className="font-nunito text-base-subtitle flex items-center gap-2 hover:underline"
-            >
-              <Users size={18} weight="fill" className="text-base-label" />
-              32 seguidores
-            </a>
+              {userInfo?.company}
+            </li>
+          )}
+          <li className="font-nunito text-base-subtitle flex items-center gap-2">
+            <Users size={18} weight="fill" className="text-base-label" />
+            {userInfo?.followers} seguidores
           </li>
         </ul>
       </div>
