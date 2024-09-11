3e6cd3766140d379f53c3b03b49b2153c2197149
Pamela Santos <pamesantoss@hotmail.com>
2024-09-11 20:25:16 +0000

    feat: adding plugin to read markdown text

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,18 +1,17 @@
+import Markdown from "react-markdown"
 import { CardIntroIssue } from "../../components/CardIntroIssue"
 
 export function Article() {
+  const markdownText =
+    "Programming languages all have built-in data *structures*, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn."
+
   return (
     <>
       <CardIntroIssue />
 
       <div className="flex flex-col p-8">
         <p className="font-nunito text-base-text">
-          Programming languages all have built-in data structures, but these
-          often differ from one language to another. This article attempts to
-          list the built-in data structures available in JavaScript and what
-          properties they have. These can be used to build other data
-          structures. Wherever possible, comparisons with other languages are
-          drawn.
+          <Markdown>{markdownText}</Markdown>
         </p>
       </div>
     </>
